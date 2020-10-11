327c5de23e997efddb0df20e966fcc9d8622a80e
Birger Moell <birger.moell@gmail.com>
2020-10-11 21:43:18 +0200

    Added spinner

--- src/Dictaphone.js
+++ src/Dictaphone.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import { playAudio, generateText } from './App.js'
 
+import ClipLoader from "react-spinners/ClipLoader";
+ 
+
 const Dictaphone = () => {
   const { transcript, resetTranscript } = useSpeechRecognition()
+  const [loading, setLoading] = useState(false)
   const [sentence, setSentence] = useState("")
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
@@ -11,12 +15,14 @@ const Dictaphone = () => {
   }
 
   const getTextResponse = async (transcript) => {
+    setLoading(true)
     let words = await generateText(transcript)
     console.log("the words are", words)
     if (words && words.text) {
         console.log("inside words")
         playAudio(words.text)
         setSentence(words.text)
+        setLoading(false)
     }
   }
 
@@ -28,8 +34,13 @@ const Dictaphone = () => {
 
       <p>{transcript}</p>
       <hr></hr>
-      <button onClick={() => getTextResponse(transcript)}>Generate response</button>
-      {sentence && <div className="Sentence-container"><p>{sentence}</p></div>}
+      {!loading &&  <button onClick={() => getTextResponse(transcript)}>Generate response</button>}
+      <ClipLoader
+          size={150}
+          color={"#96E42B"}
+          loading={loading}
+        />
+      {sentence && !loading && <div className="Sentence-container"><p>{sentence}</p></div>}
     </div>
   )
 }
