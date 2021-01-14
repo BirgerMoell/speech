7fc520438a3ae754d0cf767c4947aae03314085b
Birger Moell <birger.moell@gmail.com>
2021-01-14 11:38:43 +0100

    Added express server

---
+++ express.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 443
+
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/ai.homelearning.se/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/ai.homelearning.se/fullchain.pem', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
+
+app.use(express.static('website'))
+app.use('/', express.static('public'))
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port, () => console.log(`Fulfillment local server listening on http://localhost:${port}`))
