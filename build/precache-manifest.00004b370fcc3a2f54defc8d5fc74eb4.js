007264389e83bddbb1518cb6f9c59a40c846a2f8
Birger Moell <birger.moell@gmail.com>
2022-03-07 16:56:54 +0100

    Added build

---
+++ build/precache-manifest.00004b370fcc3a2f54defc8d5fc74eb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed5218936cde601d76acb2b9634531f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0fe53a44000d3af2a8f",
+    "url": "/static/css/main.5794410f.chunk.css"
+  },
+  {
+    "revision": "99e2139751b3cd1c37de",
+    "url": "/static/js/2.f38f066d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f38f066d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fe53a44000d3af2a8f",
+    "url": "/static/js/main.8866531a.chunk.js"
+  },
+  {
+    "revision": "403dba4a811b7f5e78c7",
+    "url": "/static/js/runtime-main.d9105890.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
